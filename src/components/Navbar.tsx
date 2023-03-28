359f31ac7199e49026ba210c443e8f8394b78f04
Ecstasy <ecstasyit@gmail.com>
2023-03-28 11:38:35 +0330

    Dark/Ligh mode implemented.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack padding="10px" justifyContent="space-between">
       <Image width="60px" src={logo} />
+      <ColorModeSwitch />
     </HStack>
   );
 };
